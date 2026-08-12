100723f719217b6bfb6a775ace8023a0d6f44f48
harsh1904j <harsh19april@gmail.com>
2026-08-12 17:46:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Count Reverse Pairs/count-reverse-pairs.cpp
@@ -0,0 +1,52 @@
+class Solution {
+  public:
+  int cnt = 0;
+ void merge(vector<int>& arr, int low, int mid, int high) {
+        vector<int> temp;
+        int left = low, right = mid + 1;
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right])
+                temp.push_back(arr[left++]);
+            else
+                temp.push_back(arr[right++]);
+        }
+        while (left <= mid)
+            temp.push_back(arr[left++]);
+
+
+        while (right <= high)
+            temp.push_back(arr[right++]);
+
+        for (int i = low; i <= high; i++)
+            arr[i] = temp[i - low];
+    }
+    void countPairs(vector<int>& nums, int low, int mid, int high){
+        int right = mid + 1;
+        for(int i = low; i <= mid; i++){
+            while(right <= high && (long long)nums[i] > 2 * (long long)nums[right]) right++;
+            cnt += (right - (mid + 1));
+        }
+    }
+
+    void mergeSort(vector<int>& arr, int low, int high) {
+        if (low >= high)
+            return;
+
+        int mid = (low + high) / 2;
+
+        mergeSort(arr, low, mid);
+
+        mergeSort(arr, mid + 1, high);
+        
+        countPairs(arr, low, mid , high);
+
+        merge(arr, low, mid, high);
+    }
+    int countRevPairs(vector<int> &arr) {
+        int n = arr.size();
+        mergeSort(arr, 0, n - 1);
+        return cnt;
+        
+    }
+};
