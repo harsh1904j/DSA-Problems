a1e7619137c8e07431208e7e69d850c8e7748650
harsh1904j <harsh19april@gmail.com>
2026-08-21 21:30:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Capacity To Ship Packages Within D Days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    int leastWeightCapacity(vector<int>& weight, int days) {
+        int n = weight.size();
+        int low = *max_element(weight.begin(), weight.end());
+        int high = accumulate(weight.begin(), weight.end(), 0);
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            int daysReq = 1, load = 0;
+            for(int i = 0; i < n; i++){
+                if(load + weight[i] > mid){
+                    daysReq++;
+                    load = weight[i];
+                }
+                else{
+                    load += weight[i];
+                }
+            }
+            if(daysReq <= days){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return low;
+        
+    }
+};
