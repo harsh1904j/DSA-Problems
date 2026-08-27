00304f4b620e60d36056b7a2244fe7e033a029b0
harsh1904j <harsh19april@gmail.com>
2026-08-27 20:55:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Outermost Parentheses of Primitives/outermost-parentheses-of-primitives.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    string removeOuter(string& s) {
+        string x = "";
+        int cnt = 0;
+        for(char ch : s){
+            if(ch == '('){
+                if(cnt > 0) x += ch;
+                cnt++;
+            }
+            else{
+                cnt--;
+                if(cnt > 0) x += ')';
+            }
+        }
+        return x;
+        
+    }
+};
