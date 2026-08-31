289924be7fb46497548bd20b5f5066e3468979cc
harsh1904j <harsh19april@gmail.com>
2026-08-31 22:10:39 +0530

    Time: 15 ms (9.42%) | Memory: 13.3 MB (37.83%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -6,7 +6,7 @@ public:
             };
         int n = s.size();
         int ans = 0;
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n-1; i++){
             if(rom[s[i]]<rom[s[i+1]]){
                 ans -= rom[s[i]];
             }
@@ -14,6 +14,6 @@ public:
                 ans += rom[s[i]];
             }
         }
-        return ans;
+        return ans + rom[s.back()];
     }
 };
